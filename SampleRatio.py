d9daa147bec31e9ad935b5e4dfd9b23ef9d6367c
迮依谈 <zeyitan1998@sina.com>
2019-05-02 11:41:31 -0700

    sax initial implementation

--- SampleRatio.py
+++ SampleRatio.py
@@ -5,6 +5,7 @@ import numpy
 from tslearn.piecewise import SymbolicAggregateApproximation
 from tslearn.utils import to_time_series
 
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 
 def main():
     influx_url = "http://localhost:8086/query?db=test_quarter&epoch=ms&q=SELECT+%22degrees%22+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329650000ms"
@@ -19,11 +20,11 @@ def main():
     n_segments = len(lst2)
     ts = to_time_series(lst2)
     
-    alphabet_size_avg = 5
+    alphabet_size_avg = 20
     sax = SymbolicAggregateApproximation(n_segments, alphabet_size_avg)
-    
-    sax_data = sax.transfrom(ts)
-
+    scalar = TimeSeriesScalerMeanVariance(mu=0., std=1.)
+    sdb = scalar.fit_transform(lst2)
+    sax_data = sax.transform(sdb)
  #   sax_data = sax.inverse_transform(sax.fit_transfrom(lst2))
     print(sax_data.shape)
     print(sax_data)
