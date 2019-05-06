56fe8685f7d4a5d1f96caf2edaabd49815639f48
Jiahao Xu <jiahaox3@uci.edu>
2019-05-06 16:30:01 -0700

    modify edge conditions

--- SampleRatio.py
+++ SampleRatio.py
@@ -9,13 +9,18 @@ from tslearn.utils import to_time_series
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 from tslearn.preprocessing import TimeSeriesResampler
 
+dbname = "NOAA_water_database"
 
 def main():
-    influx_url = "http://localhost:8086/query?db=test_quarter&epoch=ms&q=SELECT+%22degrees%22+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329650000ms"
+# fetch original data
+    influx_url = "http://localhost:8086/query?db=" + dbname + \
+                 "&epoch=ms&q=SELECT+%22degrees%22+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329650000ms"
     r = requests.get(influx_url)
     json_dict = json.loads(r.content)
 
     data = json_dict["results"][0]["series"][0]["values"]
+    time_interval = data[1][0] - data[0][0] # consistant time interval
+    print("time interval: ", time_interval)
     print(data)
    
     lst2 = [item[1] for item in data]
@@ -30,34 +35,36 @@ def main():
     print(sax_data.shape)
     print(sax_data)
 
-##    sample_fill_url = "http://localhost:8086/query?db=test_quarter&epoch=ms&q=SELECT+sample%28%22degrees%22%2C10%29+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329650000ms+GROUP+BY+time(1s)+fill(linear)"
-    sample_url = "http://localhost:8086/query?db=test_quarter&epoch=ms&q=SELECT+sample%28%22degrees%22%2C10%29+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329650000ms"
+# generate sample data 
+    sample_url = "http://localhost:8086/query?db="+dbname+\
+                 "&epoch=ms&q=SELECT+sample%28%22degrees%22%2C10%29+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329650000ms"
     r2 = requests.get(sample_url)
     json_dict2 = json.loads(r2.content)
-    sampled_data = json_dict2["results"][0]["series"][0]["values"]
+    sampled_data = json_dict2["results"][0]["series"][0]["values"] # [[time, value], ...]
     print(sampled_data)
    
-    sample = [item[1] for item in sampled_data]
+    sample = [item[1] for item in sampled_data] #[value,...]
     print(sample)
 
     start_x = data[0][0]
-
     end_x = data[-1][0]
     current_x = start_x
     current_loc = 0
-    slope = (sampled_data[current_loc][1]-sampled_data[current_loc+1][1])/(sampled_data[current_loc][0] - sampled_data[current_loc+1][0])
-    intersection = sampled_data[current_loc][1]-slope*sampled_data[current_loc][0]
     
+    slope = (sampled_data[current_loc][1]-sampled_data[current_loc+1][1])\
+            /(sampled_data[current_loc][0] - sampled_data[current_loc+1][0])
+    intersection = sampled_data[current_loc][1]-slope*sampled_data[current_loc][0]
+
     sample_fit = []
     while current_x <= end_x:
-        if current_x>=sampled_data[current_loc+1][0] and current_loc+1 < len(sampled_data)-2:
-            print(current_x, current_loc)
+        if current_x>=sampled_data[current_loc+1][0] and current_loc+1 < len(sampled_data)-1:
             current_loc+=1
-            slope = (sampled_data[current_loc][1]-sampled_data[current_loc+1][1])/(sampled_data[current_loc][0] - sampled_data[current_loc+1][0])
+            slope = (sampled_data[current_loc][1]-sampled_data[current_loc+1][1]) \
+                    /(sampled_data[current_loc][0] - sampled_data[current_loc+1][0])
             intersection = sampled_data[current_loc][1]-slope*sampled_data[current_loc][0]
         
         sample_fit.append([current_x, slope*current_x+intersection])
-        current_x += 1000 #1000ms
+        current_x += time_interval #1000ms
 
     print(sample_fit)
     
@@ -82,11 +89,8 @@ def main():
     plt.subplot(2,2,1)
     x = [item[0] for item in sample_fit]
     y = [item[1] for item in sample_fit]
-##    plt.plot(sdb[0].ravel(),'b-')
     plt.plot(x,y,'bo-')
-   
-##    plt.plot(sample,'r-')
-    plt.title("raw data")
+    plt.title("filled data")
 
     plt.subplot(2,2,2)
 ##    plt.plot(sdb2[0].ravel(),'bo')
@@ -94,7 +98,11 @@ def main():
     y = [item[1] for item in sampled_data]
     plt.plot(x,y,'bo-')
     plt.title("sample data")
-##    
+##
+    plt.subplot(2,2,3)
+    plt.plot(lst2,'bo-')
+    plt.title("original dataset")
+
     plt.tight_layout()
     plt.show()
     
