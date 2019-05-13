e7dfcbbd59aece7a9ac88a79a7536ad5a43c6f08
迮依谈 <zeyitan1998@sina.com>
2019-05-13 13:32:49 -0700

    edge bux fixed"

--- SampleRatio.py
+++ SampleRatio.py
@@ -12,13 +12,15 @@ from tslearn.preprocessing import TimeSeriesResampler
 
 
 ##dbname = "NOAA_water_database"
-dbname = "test3"
+dbname = "test_quarter"
+##dbname = "sinedb"
 
 def main():
 # fetch original data
     #for test_quarter db
     influx_url = "http://localhost:8086/query?db=" + dbname + \
-                 "&epoch=ms&q=SELECT+%22degrees%22+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546355705400ms+and+time+%3C%3D+1548969305400ms"
+                 "&epoch=ms&q=SELECT+%22degrees%22+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329900000ms"
+
     #FOR NOAA DB
 ##    influx_url = "http://localhost:8086/query?db=" + dbname + \
 ##                 "&epoch=ms&q=SELECT+%22degrees%22+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1439856000000ms+and+time+%3C%3D+1439992520000ms+and%28%22location%22+%3D+%27santa_monica%27%29"
@@ -27,8 +29,8 @@ def main():
     json_dict = json.loads(r.content)
 
     data = json_dict["results"][0]["series"][0]["values"]
-    print(data[0])
-    print(data[1])
+##    print(data[0])
+##    print(data[1])
     time_interval = data[1][0] - data[0][0] # consistant time interval
     print("time interval: ", time_interval)
    
@@ -38,38 +40,28 @@ def main():
     print("original data size", len(lst2))
     alphabet_size_avg = 20
 
-    sax = SymbolicAggregateApproximation(n_segments, alphabet_size_avg)
-    scalar = TimeSeriesScalerMeanVariance(mu=0., std=1.)
-    
-    sdb = scalar.fit_transform(lst2)
-    sax_data = sax.transform(sdb)
-#    print(sax_data)
-    s3 = sax.fit_transform(sax_data)
-    print("s3")
- #   print(s3)
-
 # generate sample data
     sample_size = 50
-    
+    sample_url = "http://localhost:8086/query?db="+dbname+\
+                 "&epoch=ms&q=SELECT+sample%28%22degrees%22%2C" + str(sample_size) +\
+                 "%29+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546329600000ms+and+time+%3C%3D+1546329900000ms"
+
 ##    sample_url = "http://localhost:8086/query?db=" + dbname + \
 ##                 "&epoch=ms&q=SELECT+sample%28%22degrees%22%2C" + str(sample_size) +\
 ##                 "%29+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1439856000000ms+and+time+%3C%3D+1442612520000ms+and%28%22location%22+%3D+%27santa_monica%27%29"
-    sample_url = "http://localhost:8086/query?db="+dbname+\
-             "&epoch=ms&q=SELECT+sample%28%22degrees%22%2C" + str(sample_size) +\
-             "%29+FROM+%22h2o_temperature%22+WHERE+time+%3E%3D+1546355705400ms+and+time+%3C%3D+1548969305400ms"
-
-    
+   
     r2 = requests.get(sample_url)
     json_dict2 = json.loads(r2.content)
     sampled_data = json_dict2["results"][0]["series"][0]["values"] # [[time, value], ...]
 
+
+    
     print("sample length")
     print(len(sampled_data))
    
     sample = [item[1] for item in sampled_data] #[value,...]
    # print(sample)
     
-
     start_x = data[0][0]
     end_x = data[-1][0]
     current_x = start_x
@@ -80,52 +72,60 @@ def main():
     intersection = sampled_data[current_loc][1]-slope*sampled_data[current_loc][0]
 
     sample_fit = []
-    
-    while current_x <= end_x:
+    end_sample_x = sampled_data[-1][0]
+
+    while current_x <= end_sample_x:
+    #while current_x <= end_x:
         #print(current_x)
-        ## if current x
-        if current_x>=sampled_data[current_loc+1][0] and current_loc+1 < len(sampled_data)-1:
+        #if current_x == sampled_data[-1][0]:           
+        if current_x >= sampled_data[current_loc+1][0] and current_loc+1 < len(sampled_data)-1:
             current_loc+=1
             slope = (sampled_data[current_loc] [1]-sampled_data[current_loc+1][1]) \
                     /(sampled_data[current_loc][0] - sampled_data[current_loc+1][0])
             intersection = sampled_data[current_loc][1] - slope*sampled_data[current_loc][0]
         
+        
         sample_fit.append([current_x, slope*current_x+intersection])
         current_x += time_interval #1000ms
-    
-    print("sample_fit")
-    print(sample_fit[0])  #sampled data
-    print(sample_fit[1])
+    #to-do: chopped the original data to match the linear fit sample data.
+
+    chopped_data = []
+    for item in data:
+        if item[0]>= sample_fit[0][0] and item[0] <= sample_fit[-1][0]:
+            chopped_data.append(item)
+    print("len")
+    print(len(sample_fit),len(chopped_data))
+    chopped_lst2 = [item[1] for item in chopped_data]
+    chopped_len = len(chopped_lst2)
+
+    sax = SymbolicAggregateApproximation(chopped_len,alphabet_size_avg)
+    scalar = TimeSeriesScalerMeanVariance(mu=0., std=1.)    
+    sdb = scalar.fit_transform(chopped_lst2)
+    sax_data = sax.transform(sdb)
+    s3 = sax.fit_transform(sax_data)
 
     sample_fit_extract = [item[1] for item in sample_fit]
     fit_sample_data = scalar.fit_transform(sample_fit_extract)
  
     sax_sample_data = sax.transform(fit_sample_data)
     s4 = sax.fit_transform(sax_sample_data)
+
     print("distance")
     dist = sax.distance_sax(s3[0], s4[0])
     print(dist)
     print("normalized distance")
-    print(dist/math.sqrt(sample_size))
-    
-    
+    print(dist/chopped_len)
 
-##    sdb2 = scalar.fit_transform(TimeSeriesResampler(sz=len(lst2)).fit_transform(sample))
-##    sdb2 = scalar.fit_transform(sample)
-##    sax_sample = sax.transform(sdb2)
-
-##    print(sax_sample.shape)
-##    print(sax_sample)
-##    
-##    print("distance")
-##    print(sax.distance(sax_data, sax_sample))
-##
+    
     plt.figure()
     plt.subplot(2,2,1)
     x = [item[0] for item in sample_fit]
     y = [item[1] for item in sample_fit]
     plt.plot(x,y,'bo-')
-    plt.ylim(top = 12,bottom = -12)
+
+
+    # to temporary fix the edge issue
+    #plt.ylim(top = 82, bottom = 58)
     plt.title("sample data (linear fill)")
 
     plt.subplot(2,2,2)
