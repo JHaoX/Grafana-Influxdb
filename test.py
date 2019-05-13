6b2498bce86fbbf0b104414e8cc00f2055a78e1f
Jiahao Xu <jiahaox3@uci.edu>
2019-05-13 12:54:22 -0700

    add comment

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+## This file is used to generate random time series data, which can be
+##   imported by influxdb:
+##     influx -import -path=FILE_NAME -precision=s -database=DB_NAME
+
+
 import random
 import math
 
@@ -9,7 +14,9 @@ print("# DML")
 
 print("# CONTEXT-DATABASE: sinedb")
 
+## The range is the time range w/ unix time stamp 
 for i in range(1546300800, 1546948800):
-	print("h2o_temperature,location=santa_monica degrees={} {}".format(10*math.sin(i/10000) + random.randint(-3,3),i))
+	print("h2o_temperature,location=santa_monica degrees={} {}"\
+              .format(10*math.sin(i/10000) + random.randint(-3,3),i))
 
 
